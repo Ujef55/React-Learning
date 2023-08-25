72478b880b67813f51388652b4d89faa2bb3594a
Ujef55 <ujef.kelawala123@gmail.com>
2023-08-25 20:38:42 +0530

    Added props

--- 03Props and tailwind css/src/App.jsx
+++ 03Props and tailwind css/src/App.jsx
@@ -1,7 +1,7 @@
-function App() {
+function App(props) {
   return (
     <>
-      <h1 class="text-4xl bg-blue-400 m-5 p-5 w-72 rounded-xl shadow-xl mx-auto text-center">Tailwind CSS</h1>
+      <h1 className="text-4xl hover:bg-blue-400 bg-blue-500 m-5 p-5 w-72 rounded-xl shadow-xl mx-auto text-center cursor-pointer">{props.title}</h1>
     </>
   );
 }
