c53b221675adb3d4981c580c4d3c6c944480fcee
Ujef55 <ujef.kelawala123@gmail.com>
2023-08-25 17:42:21 +0530

    tailwind install and testing

---
+++ 03Props and tailwind css/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='bg-blue-400 text-white p-4 m-4 text-center w-32 rounded-xl'>Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
