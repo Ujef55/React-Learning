4ebca264bc1d18dcba2763be8d85c0fa744bd67f
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-01 18:11:19 +0530

    Fixing logic

--- 05Password-generator/src/App.jsx
+++ 05Password-generator/src/App.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [length, setLength] = useState(6)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const randomPassword = useEffect(() => {
+    let pass = '';
+    let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if(numberAllowed) {
+      string += '0123456789';
+    }
+
+    if(charAllowed) {
+      string += '!@#$%^&*(){}[]:"<>?';
+    }
+
+    for(let i = 1; i <= length; i++ ) {
+      const randomLetter = Math.floor(Math.random() * string.length + 1);
+      pass += string.charAt(randomLetter);
+    }
+
+    setPassword(pass);
+
+  }, [length, numberAllowed, charAllowed])
 
   return (
    <>
-      <h1>Hello from react</h1>
+     
    </>
   )
 }
