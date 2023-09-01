b91149003999b7a85e91d38fb0449faa0c4cd0ee
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-02 02:33:31 +0530

    Length, number and char functionality added

--- 05Password-generator/src/App.jsx
+++ 05Password-generator/src/App.jsx
@@ -1,37 +1,97 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import "./index.css";
 
 function App() {
-  const [length, setLength] = useState(6)
-  const [numberAllowed, setNumberAllowed] = useState(false)
-  const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState('')
+  const [length, setLength] = useState(6);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const randomPassword = useEffect(() => {
-    let pass = '';
-    let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const randomPassword = () => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if(numberAllowed) {
-      string += '0123456789';
+    if (numberAllowed) {
+      string += "0123456789";
     }
 
-    if(charAllowed) {
+    if (charAllowed) {
       string += '!@#$%^&*(){}[]:"<>?';
     }
 
-    for(let i = 1; i <= length; i++ ) {
+    for (let i = 1; i <= length; i++) {
       const randomLetter = Math.floor(Math.random() * string.length + 1);
       pass += string.charAt(randomLetter);
     }
 
     setPassword(pass);
+  };
 
-  }, [length, numberAllowed, charAllowed])
+  useEffect(() => {
+    randomPassword();
+  }, [length, numberAllowed, charAllowed]);
 
   return (
-   <>
-     
-   </>
-  )
+    <div className="bg-black w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-white text-2xl text-center p-6">
+        Random Password Generator
+      </h1>
+      <div className="bg-gray-800 p-32 w-8/12 rounded-xl flex flex-col items-center">
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white py-1 px-3">
+            Copy
+          </button>
+        </div>
+
+        <div className="flex text-white items-center m-7 gap-4">
+          <div className="flex items-center">
+            <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+            />
+          <label>Length: {length}</label>
+          </div>
+
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
